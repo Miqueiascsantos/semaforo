d3844fe6ff6883f738a6c25b3257279e6c0fa756
Miqueiascsantos <87588124+Miqueiascsantos@users.noreply.github.com>
2021-08-31 09:54:33 -0300

    Add files via upload

---
+++ Semafaro_com_serial.ino
@@ -0,0 +1,38 @@
+void setup() {
+
+  pinMode(10,OUTPUT); //vermelho
+  pinMode(11,OUTPUT); //verde
+  pinMode(12,OUTPUT); //amarelo
+  Serial.begin(9600);
+}
+
+ 
+void loop() 
+{
+  char v = Serial.read();
+  
+  if (v == 'a')
+  {
+    digitalWrite(10, HIGH);
+    delay(2000);
+    digitalWrite(10, LOW);
+  }
+  
+  
+  else if (v == 's')
+  {
+    digitalWrite(11, HIGH);
+    delay(3000);
+    digitalWrite(11, LOW);
+  }
+
+  
+  else if (v == 'd')
+  {
+    digitalWrite(12, HIGH);
+    delay(1000);
+    digitalWrite(12, LOW);
+  }
+   
+}
+
---
+++ Semaforo.ino
@@ -0,0 +1,34 @@
+void setup() 
+{
+  
+  pinMode(10,OUTPUT); //vermelho
+  pinMode(11,OUTPUT); //verde
+  pinMode(12,OUTPUT); //amarelo
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+
+  char v = Serial.read();
+
+  switch(v)
+  {
+    case 'a': 
+    digitalWrite(10, HIGH);
+    delay(2000);
+    digitalWrite(10, LOW);
+
+    case 's':
+    digitalWrite(11, HIGH);
+    delay(3000);
+    digitalWrite(11, LOW);
+
+    case 'd':
+    digitalWrite(12, HIGH);
+    delay(1000);
+    digitalWrite(12, LOW);
+  }
+  
+}
